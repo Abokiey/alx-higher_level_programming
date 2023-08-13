efca5a4b651630b5adda27dccff8c549c161fbb0
Abokiey <sweneysam@gmail.com>
2023-08-13 12:38:30 +0300

    new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    list_cpy = my_list[:]
+
+    elem_no = len(list_cpy)
+    if idx < 0:
+        return list_cpy
+    if idx >= elem_no
+        return list_cpy
+    list_cpy[idx] = element
+    return list_cpy
