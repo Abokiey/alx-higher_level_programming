a43fcaf010877614c5e1e2f50aab020bafa8ba04
Abokiey <sweneysam@gmail.com>
2023-08-29 17:52:53 +0300

    decorators

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""class of a square"""
+
+
+class Square:
+    """initialize"""
+    def __init__(self, size):
+        """conditions"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """property setter"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """property setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """area of a square"""
+        return (self.size ** 2)
