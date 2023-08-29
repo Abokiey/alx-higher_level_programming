aaaafa90dc37d83a153a17e5801f5b356b077dcc
Abokiey <sweneysam@gmail.com>
2023-08-29 18:17:43 +0300

    square ##

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+
+"""class of a square"""
+
+
+class Square:
+    """initialize"""
+    def __init__(self, size):
+        self.__size = size
+
+    @property
+    def size(self):
+        """property setter"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """property setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """area of a square"""
+        return (self.size ** 2)
+
+    def my_print(self):
+        """prints the # character"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                [print("#", end="")for j in range(self.__size)]
+                print("")
