3509fb24b243d71cd58af697c4498de037d5448c
Abokiey <sweneysam@gmail.com>
2023-08-29 17:57:46 +0300

    decorators

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,13 +6,7 @@
 class Square:
     """initialize"""
     def __init__(self, size):
-        """conditions"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
