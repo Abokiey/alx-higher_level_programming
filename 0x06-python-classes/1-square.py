1fad3df694251afb0a2d8b2185329aae56355dc6
Abokiey <sweneysam@gmail.com>
2023-08-29 14:00:55 +0300

    private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""square class"""
+
+
+
+class Square:
+    """initializing self"""
+    def __init__(self, size):
+
+        """make a private attribute"""
+
+        self.__size = size
