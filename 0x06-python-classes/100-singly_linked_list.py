9529aae67bc68b0fc809f02755d792a362cd4e4c
Abokiey <sweneysam@gmail.com>
2023-08-30 17:36:15 +0300

    singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -8,13 +8,13 @@ class Node:
     """node of a linked list"""
     def __init__(self, data, next_node=None):
         """initialize"""
-        self.data = data
-        self.next_node = next_node
+        self.__data = data
+        self.__next_node = next_node
 
     @property
     def data(self):
         """getter"""
-        return self.data
+        return self.__data
 
     @data.setter
     def data(self, value):
@@ -44,6 +44,17 @@ class SinglyLinkedList:
     def __init__(self):
         self.__head = None
 
+    def __str__(self):
+        """singly linked list needed to print"""
+        string = ""
+        tmp = self.__head
+        while tmp is not None:
+            string += str(tmp.data)
+            tmp = tmp.next_node
+            if tmp is not None:
+                string += "\n"
+        return string
+
     def sorted_insert(self, value):
 
         """initialize"""
@@ -54,12 +65,12 @@ class SinglyLinkedList:
 
         tmp = self.__head
         if new.data < tmp.data:
-            new.next_node = self.__head
+            new.__next_node = self.__head
             self.__head = new
             return
 
         while (tmp.next_node is not None) and (new.data > tmp.next_node.data):
             tmp = tmp.next_node
-        new.next_node = tmp.next_node
-        tmp.next_node = new
+        new.__next_node = tmp.next_node
+        tmp.__next_node = new
         return
