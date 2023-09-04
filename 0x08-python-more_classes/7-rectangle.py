c26df0bace44b10717e488e4f2966b99eb542f94
Abokiey <sweneysam@gmail.com>
2023-09-04 17:56:42 +0300

    symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return (string)
         for i in range(self.height):
-            [res.append('#') for j in range(self.__width)]
+            [res.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 res.append("\n")
         return ("".join(res))
