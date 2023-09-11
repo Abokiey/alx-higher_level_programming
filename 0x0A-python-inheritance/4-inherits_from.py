5b3244b5af0fd6c14676ea4154d071822e5715b6
Abokiey <sweneysam@gmail.com>
2023-09-11 18:33:21 +0300

    sa

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,6 @@ def inherits_from(obj, a_class):
     :a_class:the class to compare against
     :return: if the object is an instance of the class
     """
-    return issubclass(type(obj), a_class)
+    if getattr(obj, "__class__") == a_class:
+        return False
+    return isinstance(obj, a_class)
