25765649724b82703c74bf89d07b1566ef08a7e0
Abokiey <sweneysam@gmail.com>
2023-09-11 19:59:53 +0300

    instance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,7 +17,7 @@ class BaseGeometry:
         :raises TypeError: <name> must be an integer
         :raises ValueError: <name> must be greater than 0
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
