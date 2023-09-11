f41c89a5fe204341f4676ebb9172d4d96de4aa12
Abokiey <sweneysam@gmail.com>
2023-09-11 18:23:38 +0300

    instance of a class that inherited (directly or indirectly)

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def inherits_from(obj, a_class):
+    """
+    :if the object is an instance of a class that inherited
+    :obj: object to check
+    :a_class:the class to compare against
+    :return: if the object is an instance of the class
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
