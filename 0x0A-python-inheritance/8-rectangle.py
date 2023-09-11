a2e806485f5032974645125d300de7ca1f5e7e47
Abokiey <sweneysam@gmail.com>
2023-09-11 20:43:35 +0300

    7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,7 +17,7 @@ class BaseGeometry:
         :raises TypeError: <name> must be an integer
         :raises ValueError: <name> must be greater than 0
         """
-        if not isinstance(value, int)
+        if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
