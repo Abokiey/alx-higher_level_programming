051bc8817633c8cb79b751372897f0476d7655a6
Abokiey <sweneysam@gmail.com>
2023-09-12 19:42:01 +0300

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,3 +16,14 @@ def pascal_triangle(n):
         row.append(1)
         triangle.append(row)
     return triangle
+
+
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
