8f93115969ba12b4f85fee4034933aa811aa124b
Abokiey <sweneysam@gmail.com>
2023-09-12 17:49:34 +0300

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,10 @@
 
 """import modules"""
 import sys
+import json
+
+"""local import"""
+
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
