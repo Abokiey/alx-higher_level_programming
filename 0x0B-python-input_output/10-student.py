24b70814d427d9234c32c155b19a300ebe490855
Abokiey <sweneysam@gmail.com>
2023-09-12 19:10:52 +0300

    10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -12,7 +12,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """retrieves the dictionary representation"""
-        if attr is None:
+        if attrs is None:
             return (self.__dict__)
         else:
             dictionary = {}
@@ -21,3 +21,16 @@ class Student:
                     dictionary[at] = self.__dict__[at]
             return (dictionary)
 
+if __name__ == "__main__":
+    
+
+    student_1 = Student("John", "Doe", 23)
+    student_2 = Student("Bob", "Dylan", 27)
+
+    j_student_1 = student_1.to_json()
+    j_student_2 = student_2.to_json(['first_name', 'age'])
+    j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+    print(j_student_1)
+    print(j_student_2)
+    print(j_student_3)
