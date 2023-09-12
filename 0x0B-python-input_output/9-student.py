eea559baca75c09936723f274c20095b46234b07
Abokiey <sweneysam@gmail.com>
2023-09-12 18:52:49 +0300

    9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,21 +10,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    """json method"""
-
-
     def to_json(self):
         """retrieves the dictionary representation"""
         return (self.__dict__)
-
-
-if __name__ == "__main__":
-    students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
