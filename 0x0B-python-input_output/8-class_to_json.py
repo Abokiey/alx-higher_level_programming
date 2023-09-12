0722eaeda05d169dd8d23b9756d0a99be52971c4
Abokiey <sweneysam@gmail.com>
2023-09-12 18:15:37 +0300

    8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""import module"""
+
+import json
+
+"""return dictionary from a simple data structures """
+
+
+def class_to_json(obj):
+    """initialization"""
+    return (obj.__dict__)
