4335b46b071886de63bbbba26c73f962d1ade8c4
Abokiey <sweneysam@gmail.com>
2023-09-12 14:11:47 +0300

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""writing on a file"""
+
+
+def write_file(filename="", text=""):
+    """writing a string"""
+    length = 0
+    with open(filename, 'w', encoding="utf-8") as f:
+        length = f.write(text)
+    return (length)
+
+
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
