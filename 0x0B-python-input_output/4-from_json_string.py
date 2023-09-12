31f022c37ca05427ef20d30b3d381601e8286d3d
Abokiey <sweneysam@gmail.com>
2023-09-12 15:45:23 +0300

    4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+"""json module"""
+
+import json
+
 """converts from json str to py. object"""
 
 
 def from_json_string(my_str):
     """deserialization from json str"""
-
+    return (json.loads(my_str))
