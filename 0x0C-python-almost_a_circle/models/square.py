4a52d9c3a9e7b7b3aac63e8c4e71a5f2936576b2
Abokiey <sweneysam@gmail.com>
2023-09-15 15:19:01 +0300

    sqr

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,16 +29,40 @@ class Square(Rectangle):
         return ("[square] ({}) {}/{} - {}".format(self.id, self.x,
             self.y, self.size))
 
+    def update(self, *args, **kwargs):
+        """assign attributes"""
+        if args:
+            keys = ["id", "size", "x", "y"]
+            for key, value in zip(keys, args):
+                setattr(self, key, value)
+
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
 
 if __name__ == "__main__":
 
     s1 = Square(5)
     print(s1)
-    print(s1.size)
-    s1.size = 10
+
+    s1.update(10)
     print(s1)
 
-    try:
-        s1.size = "9"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    s1.update(1, 2)
+    print(s1)
+
+    s1.update(1, 2, 3)
+    print(s1)
+
+    s1.update(1, 2, 3, 4)
+    print(s1)
+
+    s1.update(x=12)
+    print(s1)
+
+    s1.update(size=7, y=1)
+    print(s1)
+
+    s1.update(size=7, id=89, y=1)
+    print(s1)
