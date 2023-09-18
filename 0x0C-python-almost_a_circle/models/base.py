351e5a4dc3a45a988cb4ed94d0ce933ab9fe8239
Abokiey <sweneysam@gmail.com>
2023-09-18 14:32:15 +0300

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """import modules"""
 
 import json
+import csv
 
 """base model class"""
 
@@ -76,17 +77,60 @@ class Base:
         except IOError:
             return []
 
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """seriallize in csv"""
+        file = "{}.csv".format(cls.__name__)
+
+        if list_objs is None:
+            return []
+        with open(file, 'w', newline='') as f:
+            writer = csv.writer(f)
+
+        for obj in list_objs:
+            if cls.__name__ == "Rectangle":
+                data = [obj.id, obj.width, obj.height, obj.x, obj.y]
+        if cls.__name__ == "Square":
+            data = [obj.id, obj.size, obj.x, obj.y]
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserialize in csv"""
+        file = "{}.csv".format(cls.__name__)
+        instances = []
+
+        with open(file, 'r', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if cls.__name__ == "Rectangle":
+                    dictionary = {"id": int(row[0]),
+                                  "width": int(row[1]),
+                                  "height": int(row[2]),
+                                  "x": int(row[3]),
+                                  "y": int(row[4])}
+                if cls.__name__ == "Square":
+                    dictionary = {"id": int(row[0]),
+                                  "size": int(row[1]),
+                                  "x": int(row[2]),
+                                  "y": int(row[3])}
+                instance = cls.create(**dictinary)
+                instances.append(instance)
+            return (instances)
+
+
 if __name__ == "__main__":
     from rectangle import Rectangle
     from square import Square
+
+if __name__ == "__main__":
 
     r1 = Rectangle(10, 7, 2, 8)
     r2 = Rectangle(2, 4)
     list_rectangles_input = [r1, r2]
 
-    Rectangle.save_to_file(list_rectangles_input)
+    Rectangle.save_to_file_csv(list_rectangles_input)
 
-    list_rectangles_output = Rectangle.load_from_file()
+    list_rectangles_output = Rectangle.load_from_file_csv()
 
     for rect in list_rectangles_input:
         print("[{}] {}".format(id(rect), rect))
@@ -103,9 +147,9 @@ if __name__ == "__main__":
     s2 = Square(7, 9, 1)
     list_squares_input = [s1, s2]
 
-    Square.save_to_file(list_squares_input)
+    Square.save_to_file_csv(list_squares_input)
 
-    list_squares_output = Square.load_from_file()
+    list_squares_output = Square.load_from_file_csv()
 
     for square in list_squares_input:
         print("[{}] {}".format(id(square), square))
