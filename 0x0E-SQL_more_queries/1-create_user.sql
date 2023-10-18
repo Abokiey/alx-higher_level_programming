b6e79c4b42b2d70eaed2f5f6971b1daf8451d3c7
Abokiey <sweneysam@gmail.com>
2023-10-18 14:23:04 +0300

    1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create a user in mysql server
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
