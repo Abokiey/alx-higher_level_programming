e41aa9b7d5ee25f326dec0486efa4ce320daffdb
Abokiey <sweneysam@gmail.com>
2023-10-18 17:43:59 +0300

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
 -- lists all cities contained in database
 
-USE hbtn_0d_usa;
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = state.id
-ORDER BY cities.id ASC;
+SELECT  cities.id, cities.name, states.name FROM cities
+JOIN states
+	ON cities.state_id = states.id;
