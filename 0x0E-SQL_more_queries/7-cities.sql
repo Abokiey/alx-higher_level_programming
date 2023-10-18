821f9c9d450cfb2ec17b8a8f26f3367baa4be9f0
Abokiey <sweneysam@gmail.com>
2023-10-18 16:16:28 +0300

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates a database and a table with consntraints
 
 CREATE	DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE	TABLE IF NOT EXISTS  hbtn_0d_usa.cities(
+CREATE	TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
