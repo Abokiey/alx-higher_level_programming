72c132cea40053e53a681739a0cb2404e5dd0169
Abokiey <sweneysam@gmail.com>
2023-11-13 15:13:05 +0300

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,14 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0)
+{
   console.log('No argument');
+}
 if (args.length === 1)
+{
   console.log('Argument found');
+}
 if (args.length > 1)
+{
   console.log('arguments found');
+}
