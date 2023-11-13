115472237c1015b7be93ed5d5a0d65336530c901
Abokiey <sweneysam@gmail.com>
2023-11-13 16:55:19 +0300

    6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+
 /* prit contents of an array*/
 
-const arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const ar of arr)
+for (const i of arr)
 {
-  console.log(ar);
+  console.log(i);
 }
