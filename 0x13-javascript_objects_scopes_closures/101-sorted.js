942ab2b0c00754b1f2577b13fe210d660fb6c3a5
Abokiey <sweneysam@gmail.com>
2023-11-14 19:08:44 +0300

    102

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const ndict = {};
+for (const key in dict) {
+  if (ndict[dict[key]] === undefined) {
+    ndict[dict[key]] = [key];
+  } else {
+    ndict[dict[key]].push(key);
+  }
+}
+console.log(ndict);
