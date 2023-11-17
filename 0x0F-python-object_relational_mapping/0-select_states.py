d46df456b66b3e5064dd3cdc881fdeb6ccd21fc7
Abokiey <sweneysam@gmail.com>
2023-11-17 15:57:19 +0300

    0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""import modules"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """connect to the server"""
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], password=sus.argv[2], database=sys.argv[3], port=3306)
+
+    """prepare cursor object"""
+    cursor = db.cursor()
+
+    """execute the cursor"""
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
