018fc48ea16a78d8cbe3a9750a02731c3b3528c5
Abokiey <sweneysam@gmail.com>
2023-11-18 11:40:55 +0300

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,16 +7,10 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    # connect the db using command-line arguments
-    my_db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        password=argv[2],
-        db=argv[3]
-    )
+    # connect the db
+    my_db = MySQLdb.connect(host="localhost", port=3306,
+                            user=argv[1], password=argv[2], db=argv[3])
 
-    # create the cusror && execute the query
     my_cursor = my_db.cursor()
     my_cursor.execute(
         """SELECT * FROM states WHERE name LIKE
@@ -24,15 +18,10 @@ if __name__ == "__main__":
         """
         )
 
-    # fetch the data queried
-    my_data = my_cursor.fetchall()
+    states = my_cursor.fetchall()
 
-    # iterate to print a tuple
-    for data in my_data:
-        print(data)
+    for state in states:
+        print(state)
 
-    # Close all cursors
     my_cursor.close()
-
-    # Close all databases
     my_db.close()
