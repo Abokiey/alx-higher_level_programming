9a2a1646be6fe245ee07aa314d53e52cb378d541
Abokiey <sweneysam@gmail.com>
2023-11-19 17:31:35 +0300

    100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""module imports"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """state class that inherits from base class"""
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
