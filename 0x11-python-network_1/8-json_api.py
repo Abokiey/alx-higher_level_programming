e869e73914957e6f87f8e25063000b637afe354d
Abokiey <sweneysam@gmail.com>
2023-12-01 19:38:34 +0300

    8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""takes in a letter and sends a POST request
+   to http://0.0.0.0:5000/search_user with the
+   letter as a parameter.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    resp = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    try:
+        response = resp.json()
+        if response == {}:
+            print("No result")
+        else:
+            print(f'[{response.get("id")}] {response.get("name")}')
+    except ValueError:
+        print("Not a valid JSON")    
