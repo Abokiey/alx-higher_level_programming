a284c15015ce23306186102aca648ce15ec46d3a
Abokiey <sweneysam@gmail.com>
2023-12-01 14:30:48 +0300

    1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""send a url request and display the request id"""
+
+import urllib
+from sys import argv
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        headers = response.info()
+        print(headers.get("X-Request-Id"))
