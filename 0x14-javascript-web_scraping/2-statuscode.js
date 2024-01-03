53dcf5b3c0610b4dc8683dea0b7fa70d54146985
Abokiey <sweneysam@gmail.com>
2024-01-03 16:35:58 +0300

    2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// status code from requests of  websites
+
+const request = require('request');
+const url = process.argv[2];
+
+function callback (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:' + response.statusCode);
+  }
+}
+
+request(url, callback);
