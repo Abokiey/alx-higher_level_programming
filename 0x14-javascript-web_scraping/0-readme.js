f26cfc6caaba22d7be8b2f285d1757bcfc9845a8
Abokiey <sweneysam@gmail.com>
2024-01-03 14:29:09 +0300

    0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* read and print contents of a file */
+
+const fs = require('fs');
+const path = process.argv[2];
+
+fs.readFile(path, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+})
